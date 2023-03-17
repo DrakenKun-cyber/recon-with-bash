594d1e79fbd55390dcbb144cd4a0d00586477218
Amr Mustafa <83434634+DrakenKun-cyber@users.noreply.github.com>
2023-03-17 18:12:08 +0200

    Update install.sh

--- Subdomains enumeration/install.sh
+++ Subdomains enumeration/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-go install -v github.com/projectdiscovery/katana/cmd/katana@latest;
-go install -v github.com/lc/subjs@latest;
 
-#install LinkFinder
-git clone https://github.com/GerbenJavado/LinkFinder.git; cd LinkFinder; python3 setup.py install ; pip3 install -r requirements.txt; cd ..;
-
-#install SecretFinder
-git clone https://github.com/m4ll0k/SecretFinder.git secretfinder ; cd secretfinder ; pip install -r requirements.txt ; cd .. ;
+#installation
+go install -v github.com/tomnomnom/assetfinder@latest;
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest;
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest;
